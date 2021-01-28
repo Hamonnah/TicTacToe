9f86763bcad33d2e2de196e59cd1cb1929dc200c
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2021-01-28 06:16:04 +0100

    added beter computer moves calculation

---
+++ src/main/java/TicTacToe/logic/Scores.java
@@ -0,0 +1,17 @@
+package TicTacToe.logic;
+
+public class Scores {
+
+    public static int wonGames;
+
+    public static void won() {
+        wonGames++;
+    }
+
+    public static void lost() {
+    }
+
+    public static void draw() {
+    }
+
+}
